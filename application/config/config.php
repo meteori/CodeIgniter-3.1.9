64b74a71628dd9e94a60daaa13da81782185e481
Vim <vim.jxl@gmail.com>
2018-10-22 14:45:01 +0800

    modify the config file

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 // $config['base_url'] = 'http://localhost/';
-$config['base_url'] = 'http://www.anlun.online/';
+$config['base_url'] = 'https://www.anlun.online/';
 
 /*
 |--------------------------------------------------------------------------
