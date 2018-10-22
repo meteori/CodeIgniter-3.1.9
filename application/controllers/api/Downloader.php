23854a27f6b0d1e5a7847908777070477ec495cf
Vim <vim.jxl@gmail.com>
2018-10-22 10:58:46 +0800

    optimized the file and class name

--- application/controllers/api/Downloader.php
+++ application/controllers/api/Downloader.php
@@ -14,7 +14,7 @@ require APPPATH . 'libraries/Format.php';
 * @author Vim Ji <ctc_business@163.com>
 * Date Created: 2018-10-20
 */
-class Downloader extends REST_Controller {
+class downloader extends REST_Controller {
 
   function __construct()
   {
