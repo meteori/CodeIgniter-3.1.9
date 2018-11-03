0b15f9b81c761613f2e3d9d6e28d16c12d95136b
Vim <vim.jxl@gmail.com>
2018-11-03 17:46:20 +0800

    upload common bg pic

--- application/controllers/api/Picshow.php
+++ application/controllers/api/Picshow.php
@@ -39,7 +39,7 @@ class Picshow extends REST_Controller {
                     // $files[] = $file;
                     //prepare data
                     $picUrl = base_url().'static/images/xxxshow/'.$file;
-                    $image = $picUrl;
+                    $image = base_url().'static/images/xxxshow/bg/xxxshow_bg.jpg';
                     $data = [
                       "name"=> "精品饰品", 
                       "desc"=> "精品饰品", 
